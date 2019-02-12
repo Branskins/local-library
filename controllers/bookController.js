cc2cc3fddc371909b5502127febe4d5fbad692d5
branskins <andresje17@gmail.com>
2019-02-11 23:49:00 -0600

    Handlers for update book form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -165,11 +165,111 @@ exports.book_delete_post = function(req, res) {
 };
 
 // Display book update form on GET.
-exports.book_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Book update GET');
+exports.book_update_get = function(req, res, next) {
+
+  // Get book, authors and genres.
+  async.paralleL({
+    book: function(callback) {
+      Book.findById(req.params._id).populate('author').populate('genre').exec(callback);
+    },
+    authors: function(callback) {
+      Author.find(callback);
+    },
+    genres: function(callback) {
+      Genre.find(callback)
+    }
+  }, function(err, results) {
+    if(err) { return next(err); }
+    if(book == null) { // No results.
+      var err = new Error('Book not found');
+      err.status = 404;
+      return next(err);
+    }
+    // Success.
+    // Mark our selected genres as checked.
+    for(var all_genres = 0; all_genres < results.genres.length; all_genres++) {
+      for(book_genres = 0; book_genres < book.genre.length; book_genres++) {
+        if(results.genres[all_genders]._id.toString() == results.book.genre[book_genre]._id.toString()) {
+          results.genres[all_genres].check = 'true';
+        }
+      }
+    }
+    res.render('book_form', { title: 'Update book', authors: results.authors, genres.results.genres, book: results.book });
+  });
 };
 
 // Handle book update on POST.
-exports.book_update_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Book update POST');
-};
+exports.book_update_post = [
+
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if(!(req.body.genre instanceof Array)) {
+      if(typeof req.body.genre === 'undefined')
+      req.body.genre = [];
+      else
+      req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  // Validate fields.
+  body('title', 'Title must not be empty').isLength({ min: 1 }).trim(),
+  body('author', 'Author must not be empty').isLength({ min: 1 }).trim(),
+  body('summary', 'Summary must not be empty').isLength({ min: 1 }).trim(),
+  body('isbn', 'ISBN must not be empty').isLength({ min: 1 }).trim(),
+
+  // Sanitize fields.
+  sanitizeBody('title').trim().escape(),
+  sanitizeBody('title').trim().escape(),
+  sanitizeBody('author').trim().escape(),
+  sanitizeBody('isbn').trim().escape(),
+  sanitizeBody('genre.*').trim().escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    var errors = validationResult(req);
+
+    // Create a book object with escaped/trimmed data and old id.
+    var book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: (typeof req.body.genre === 'undefined') ? [] : req.body.genre,
+      _id: req.params_id,
+    });
+
+    if(!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres from form.
+      async.parallel({
+        authors: function(callback) {
+          Author.find(callback);
+        },
+        genres: function(callback) {
+          Genre.find(callback);
+        }
+      }, function(err, results) {
+        if(err) { return next(err); }
+
+        // Mark our selected genres as checked.
+        for(let i = 0; i < results.genres; i++) {
+          if(book.genre.indexOf(results.genres[i]._id) > -1) {
+            results.genres[i].checked = 'true';
+          }
+        }
+        res.render('book_form', { title: 'Update Book', genres: results.genres, authors: results.authors, book: book, errors: errors.array() });
+      });
+    }
+    else {
+      // Data from from is valid. Update the record.
+      Book.findByIdAndUpdate(req.params.id, book, {}, function(err, thebook) {
+        if(err) { return next(err); }
+        // Succesful - redirect to book detail page.
+        res.redirect(thebook.url);
+      });
+    }
+  }
+];
