d5f3274d8d7c6cefb8b45d7f246359dfbe05ef62
branskins <andresje17@gmail.com>
2019-06-21 03:48:39 -0600

    Fix syntax errors and reference errors
    Additionally the method findByIdAndUpdate is deprecated

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -168,9 +168,9 @@ exports.book_delete_post = function(req, res) {
 exports.book_update_get = function(req, res, next) {
 
   // Get book, authors and genres.
-  async.paralleL({
+  async.parallel({
     book: function(callback) {
-      Book.findById(req.params._id).populate('author').populate('genre').exec(callback);
+      Book.findById(req.params.id).populate('author').populate('genre').exec(callback);
     },
     authors: function(callback) {
       Author.find(callback);
@@ -180,7 +180,7 @@ exports.book_update_get = function(req, res, next) {
     }
   }, function(err, results) {
     if(err) { return next(err); }
-    if(book == null) { // No results.
+    if(results.book == null) { // No results.
       var err = new Error('Book not found');
       err.status = 404;
       return next(err);
@@ -188,13 +188,13 @@ exports.book_update_get = function(req, res, next) {
     // Success.
     // Mark our selected genres as checked.
     for(var all_genres = 0; all_genres < results.genres.length; all_genres++) {
-      for(book_genres = 0; book_genres < book.genre.length; book_genres++) {
-        if(results.genres[all_genders]._id.toString() == results.book.genre[book_genre]._id.toString()) {
+      for(book_genres = 0; book_genres < results.book.genre.length; book_genres++) {
+        if(results.genres[all_genres]._id.toString() == results.book.genre[book_genres]._id.toString()) {
           results.genres[all_genres].check = 'true';
         }
       }
     }
-    res.render('book_form', { title: 'Update book', authors: results.authors, genres.results.genres, book: results.book });
+    res.render('book_form', { title: 'Update book', authors: results.authors, genres: results.genres, book: results.book });
   });
 };
 
@@ -220,7 +220,6 @@ exports.book_update_post = [
 
   // Sanitize fields.
   sanitizeBody('title').trim().escape(),
-  sanitizeBody('title').trim().escape(),
   sanitizeBody('author').trim().escape(),
   sanitizeBody('isbn').trim().escape(),
   sanitizeBody('genre.*').trim().escape(),
@@ -237,7 +236,7 @@ exports.book_update_post = [
       summary: req.body.summary,
       isbn: req.body.isbn,
       genre: (typeof req.body.genre === 'undefined') ? [] : req.body.genre,
-      _id: req.params_id,
+      _id: req.params.id,
     });
 
     if(!errors.isEmpty()) {
@@ -265,7 +264,7 @@ exports.book_update_post = [
     }
     else {
       // Data from from is valid. Update the record.
-      Book.findByIdAndUpdate(req.params.id, book, {}, function(err, thebook) {
+      Book.findOneAndUpdate({ _id: req.params.id }, book, {}, function(err, thebook) {
         if(err) { return next(err); }
         // Succesful - redirect to book detail page.
         res.redirect(thebook.url);
