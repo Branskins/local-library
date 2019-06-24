f18e1f94447ed8387a4d3452390d5fbf64b94172
branskins <andresje17@gmail.com>
2019-06-24 17:52:33 -0600

    Handlers for updating a genre
    Working correctly

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -133,10 +133,54 @@ exports.genre_delete_post = function(req, res, next) {
 
 // Display Genre update form on GET.
 exports.genre_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+
+  Genre.findById(req.params.id, function(err, genre) {
+    if(err) { return next(err); }
+    if(genre == null) {
+      var err = new Error('Genre not found');
+      err.status = 404;
+      return next(err);
+    }
+    // Success
+    // Show the genre's data
+    res.render('genre_form', { title: "Genre Update", genre: genre });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+
+  body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+  sanitizeBody('name').trim().escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    var genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id
+    });
+
+    if(!errors.isEmpty()) {
+      res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+      return;
+    }
+    else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ 'name': req.body.name })
+        .exec(function(err, found_genre) {
+          if(err) { return next(err); }
+          if(found_genre) {
+            // Genre exists, redirect to its detail page.
+            res.redirect(found_genre.url);
+          }
+          else {
+            Genre.findOneAndUpdate({ _id: req.params.id}, genre, {}, function(err, thegenre) {
+              if(err) { return next(err); }
+              res.redirect(thegenre.url);
+            });
+          }
+        });
+    }
+  }
+];
